2315597d9bffda5cbb6ba379a57e482a67f9a5b3
TheExscape <Gaetan657@hotmail.com>
2025-04-01 12:50:19 -0400

    Update Game code.py

--- Game code.py
+++ Game code.py
@@ -1,27 +1,40 @@
 import random
+import time
+
+user_text = str(input("Welcome to dice game, type roll to start game: "))
 
 
-
-input("Welcome to dice game, type roll to start game: ")
-
-
-
-dice_outcomes = [1, 2, 3, 4, 5]
-
-
-player_outcomes = "player"
-print(player_outcomes)
-print(random.randrange(1, 6)) #random for player
-
-
-
-
-computer_outcomes = "computer"
-print(computer_outcomes)
-print(random.randrange(1, 6)) #random for computer
-
-
-if computer_outcomes == player_outcomes:
-    print("tie")
+while user_text != "roll":
+    print("You did not enter roll")
+    user_text = input("please try again: ")
 else:
-    print("no")
+    print("You rolled the die")
+
+
+player_outcome = random.randrange(1, 6)
+computer_outcome = random.randrange(1, 6)
+
+print(player_outcome) #random for player
+
+for seconds in range(3, 0, -1): #this is a count down for the timer between the player roll and computer roll 
+    time.sleep(1)
+
+print()
+
+print("Computer rolls the die")
+
+
+print(computer_outcome) #random for computer
+
+for seconds in range(3, 0, -1):
+    time.sleep(1)
+
+print()
+
+if player_outcome == computer_outcome:
+    print("You guys tie")
+elif player_outcome > computer_outcome:
+    print("You win")
+elif player_outcome < computer_outcome:
+    print("You lost")
+
